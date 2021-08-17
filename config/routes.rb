0318ce5876291bc00fba31a0eef2d5869f9d1b38
hitomi314 <boc.jrock@gmail.com>
2021-08-17 12:54:35 +0000

    [Add]homes_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'homes/top'
   devise_for :users
-  root "devise/registrations#new"
+  root "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
